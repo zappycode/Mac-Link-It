8e8e364dab04645965b578a21b8b03afb310e220
Nick Walter <nick@zappycode.com>
2016-10-28 15:12:44 -0600

    Finished Version

--- Link It/AppDelegate.swift
+++ Link It/AppDelegate.swift
@@ -11,16 +11,74 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var item : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        item?.image = NSImage(named: "link")
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Link It!", action: #selector(AppDelegate.linkIt), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+        
+        item?.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func linkIt() {
+        print("WE MADE IT!")
+        
+        if let items = NSPasteboard.general().pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    if type == "public.utf8-plain-text" {
+                        if let url = item.string(forType: type) {
+                            NSPasteboard.general().clearContents()
+                            
+                            var actualURL = ""
+                            
+                            if url.hasPrefix("http://") || url.hasPrefix("https://") {
+                                actualURL = url
+                            } else {
+                                actualURL = "http://\(url)"
+                            }
+                            
+                            NSPasteboard.general().setString("<a href=\"\(actualURL)\">\(url)</a>", forType: "public.html")
+                            
+                            NSPasteboard.general().setString(url, forType: "public.utf8-plain-text")
+                            
+                            // www.zappycode.com
+                        }
+                    }
+                }
+            }
+        }
+        
+        printPasteboard()
+        
+    }
+    
+    func printPasteboard() {
+        if let items = NSPasteboard.general().pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    print("Type: \(type)")
+                    print("String: \(item.string(forType: type))")
+                }
+            }
+        }
+    }
+    
+    //www.zappycode.com
+    
+    func quit() {
+        NSApplication.shared().terminate(self)
+    }
 
 }
 
